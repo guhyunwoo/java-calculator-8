48c6dfd60d061534d0eff8de114a7b038a3763b3
guhyunwoo <realization0327@gmail.com>
2025-10-20 01:05:06 +0900

    test: Terms 단위 테스트 추가

---
+++ src/test/java/calculator/domain/TermsTest.java
@@ -0,0 +1,32 @@
+package calculator.domain;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Iterator;
+
+class TermsTest {
+
+    @Test
+    void 문자열_배열로_객체들을_정상적으로_생성해야_한다() {
+        String[] input = {"1", "2", "3"};
+        Terms terms = new Terms(input);
+
+        Iterator<Term> iterator = terms.iterator();
+
+        assertTrue(iterator.hasNext());
+        assertEquals("1", iterator.next().value());
+        assertEquals("2", iterator.next().value());
+        assertEquals("3", iterator.next().value());
+        assertFalse(iterator.hasNext());
+    }
+
+    @Test
+    void 빈_배열을_입력하면_빈_리스트가_되어야_한다() {
+        String[] input = {};
+        Terms terms = new Terms(input);
+
+        Iterator<Term> iterator = terms.iterator();
+        assertFalse(iterator.hasNext());
+    }
+}
