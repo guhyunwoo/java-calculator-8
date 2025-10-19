d791785e87a7a3809739eef8f3f452eb9a528cbd
guhyunwoo <realization0327@gmail.com>
2025-10-20 00:20:31 +0900

    test(term): 바뀐 예외처리에 맞춰 메서드 수정

--- src/test/java/calculator/domain/TermTest.java
+++ src/test/java/calculator/domain/TermTest.java
@@ -1,5 +1,8 @@
 package calculator.domain;
 
+import calculator.error.ErrorMessage;
+import calculator.error.NegativeNumberException;
+import calculator.error.NotANumberException;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -8,14 +11,14 @@ class TermTest {
     @Test
     void 음수가_입력되면_예외가_발생한다() {
         Term term = new Term("-10");
-        IllegalArgumentException ex = assertThrows(IllegalArgumentException.class, term::validateNegative);
-        assertEquals("음수는 입력할 수 없습니다.", ex.getMessage());
+        NegativeNumberException ex = assertThrows(NegativeNumberException.class, term::validateNegative);
+        assertEquals(ErrorMessage.NEGATIVE_NUMBER.getMessage(), ex.getMessage());
     }
 
     @Test
     void 숫자_형식이_아닌_문자가_입력되면_예외가_발생한다() {
         Term term = new Term("ass1093aa");
-        IllegalArgumentException ex = assertThrows(IllegalArgumentException.class, term::validateTermFormat);
-        assertEquals("숫자가 아닌 값이 입력되었습니다.", ex.getMessage());
+        NotANumberException ex = assertThrows(NotANumberException.class, term::validateTermFormat);
+        assertEquals(ErrorMessage.NOT_A_NUMBER.getMessage(), ex.getMessage());
     }
 }
