71b20f9b764823d860e587dc7730cdb2ce8afbc4
guhyunwoo <realization0327@gmail.com>
2025-10-20 00:00:38 +0900

    test(term): 문자 예외처리 유닛 테스트 추가

--- src/test/java/calculator/domain/TermTest.java
+++ src/test/java/calculator/domain/TermTest.java
@@ -11,4 +11,11 @@ class TermTest {
         IllegalArgumentException ex = assertThrows(IllegalArgumentException.class, term::validateNegative);
         assertEquals("음수는 입력할 수 없습니다.", ex.getMessage());
     }
+
+    @Test
+    void 숫자_형식이_아닌_문자가_입력되면_예외가_발생한다() {
+        Term term = new Term("ass1093aa");
+        IllegalArgumentException ex = assertThrows(IllegalArgumentException.class, term::validateTermFormat);
+        assertEquals("숫자가 아닌 값이 입력되었습니다.", ex.getMessage());
+    }
 }
