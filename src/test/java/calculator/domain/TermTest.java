233e43525c71b1a1f1af6e48505cc41ba6cb2a9f
guhyunwoo <realization0327@gmail.com>
2025-10-20 00:25:26 +0900

    test(term): 정상적인 값이 입력되었을 경우 추가

--- src/test/java/calculator/domain/TermTest.java
+++ src/test/java/calculator/domain/TermTest.java
@@ -16,9 +16,23 @@ class TermTest {
     }
 
     @Test
+    void 정상적인_값이_입력되면_음수_예외가_발생하지_않는다() {
+        Term term = new Term("10");
+        assertDoesNotThrow(term::validateNegative);
+    }
+
+    @Test
     void 숫자_형식이_아닌_문자가_입력되면_예외가_발생한다() {
         Term term = new Term("ass1093aa");
         NotANumberException ex = assertThrows(NotANumberException.class, term::validateTermFormat);
         assertEquals(ErrorMessage.NOT_A_NUMBER.getMessage(), ex.getMessage());
+    }
+
+
+
+    @Test
+    void 정상적인_값이_입력되면_문자_예외가_발생하지_않는다() {
+        Term term = new Term("100");
+        assertDoesNotThrow(term::validateTermFormat);
     }
 }
