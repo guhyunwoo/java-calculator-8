eda02ecefc09bb178184c4304e2f184b388ae565
guhyunwoo <realization0327@gmail.com>
2025-10-20 09:14:40 +0900

    test: DelimitersTest 각 메서드에서 객체를 생성하는게 아닌 객체 주입 방식 사용

--- src/test/java/calculator/domain/DelimitersTest.java
+++ src/test/java/calculator/domain/DelimitersTest.java
@@ -5,15 +5,15 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class DelimitersTest {
+    private final Delimiters delimiters = new Delimiters();
+
     @Test
     void 기본_구분자를_올바른_정규_표현식으로_반환해야_한다() {
-        Delimiters delimiters = new Delimiters();
         assertEquals(",|:", delimiters.regex());
     }
 
     @Test
     void 커스텀_구분자를_추가하고_올바른_정규_표현식으로_반환해야_한다() {
-        Delimiters delimiters = new Delimiters();
         delimiters.addCustomDelimiter("***");
         assertEquals(",|:|***", delimiters.regex());
     }
